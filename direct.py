00b8067e1ac393cadb755d24f8aaf7229938db36
YustR2hev <46708502+YustR2hev@users.noreply.github.com>
2021-04-12 15:33:40 +1000

    Update direct.py

--- direct.py
+++ direct.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/promotion_image')
-def promotion_image():
+def return_promotion_mars():
     return f"""<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/styles.css')}">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='static/css/styles.css')}">
                     <link rel="stylesheet" 
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
@@ -19,7 +19,7 @@ def promotion_image():
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/mars.png')}" 
+                    <img src="{url_for('static', filename='static/css/mars.png')}" 
                     width="300" height="300"><br>
                     <div class="alert alert-dark" role="alert">
                     <h5>Человечество вырастает из детства.<h5>
